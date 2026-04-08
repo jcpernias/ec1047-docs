936bc19e1943a22c0795d3c904906cf9d5243336
José Pernías <jcpernias@users.noreply.github.com>
2026-04-08 18:34:05 +0200

    Update code for figures

--- R/ineq-es.R
+++ R/ineq-es.R
@@ -5,20 +5,18 @@ suppressPackageStartupMessages({
 })
 
 
-var_names <- c("S80S20", "Gini", "ypc", "H", "ipc") |>
+var_names <- c("S80S20", "Gini", "ypc", "H") |>
   map(\(x) paste(x, c(2013, 2023), sep = "_")) |>
   unlist()
 
 
-es_db <- read_excel("data/ineq-data.xlsx",
+es_db <- read_excel("data/pov-ineq.xlsx",
                     col_names = c("name", var_names),
                     sheet = "CC.AA.",
-                    range = "A3:K22") |>
+                    range = "A3:I20") |>
   left_join(read_csv("data/ccaa.csv", col_types = "cc"),
             by = c("name" = "name")) |>
-  select(-name) |>
-  mutate(ypc_2013 = ypc_2013 / (10 * ipc_2013),
-         ypc_2023 = ypc_2023 / (10 * ipc_2023))
+  select(-name)
 
 es_g <- es_db |>
   mutate(
@@ -32,49 +30,28 @@ lines_db <- filter(es_g, ccaa == "ESP")
 
 mk_plt <- function(x, y) {
   es_g |>
-    filter(!ccaa %in% c("ESP", "CEU", "MEL")) |>
+    filter_out(ccaa == "ESP") |>
     ggplot(aes(x = {{ x }}, y = {{ y }})) +
-    geom_point() +
-    geom_text_repel(aes(label = ccaa)) +
-    geom_vline(aes(xintercept = {{ x }}), lines_db, color = "darkblue") +
-    geom_hline(aes(yintercept = {{ y }}), lines_db, color = "darkblue") +
-    theme_bw()
+    geom_vline(aes(xintercept = {{ x }}), lines_db, color = "snow4") +
+    geom_hline(aes(yintercept = {{ y }}), lines_db, color = "snow4") +
+    geom_point(size = 1) +
+    geom_label_repel(aes(label = ccaa), size = 4.5, box.padding = 0.2) +
+    theme_bw(base_size = 24)
 }
 
 
-ggsave("figures/ypc_H.pdf", plot = mk_plt(ypc, H),
-       width = 9, family = "Times",
-       encoding = "ISOLatin9.enc")
+save_pdf <- function(path, plot) {
+  ggsave(path, plot = plot,
+    width = 9, family = "Times",
+    encoding = "ISOLatin9.enc")
+}
 
-ggsave("figures/ypc_Gini.pdf", mk_plt(ypc, Gini),
-       width = 9, family = "Times",
-       encoding = "ISOLatin9.enc")
-
-ggsave("figures/ypc_S80S20.pdf", mk_plt(ypc, S80S20),
-       width = 9, family = "Times",
-       encoding = "ISOLatin9.enc")
-
-ggsave("figures/Gini_H.pdf", mk_plt(Gini, H),
-       width = 9, family = "Times",
-       encoding = "ISOLatin9.enc")
-
-ggsave("figures/S80S20_H.pdf", mk_plt(S80S20, H),
-       width = 9, family = "Times",
-       encoding = "ISOLatin9.enc")
-
-
-ggsave("figures/Gini.pdf", mk_plt(Gini_2013, Gini_2023),
-       width = 9, family = "Times",
-       encoding = "ISOLatin9.enc")
-
-ggsave("figures/H.pdf", mk_plt(H_2013, H_2023),
-       width = 9, family = "Times",
-       encoding = "ISOLatin9.enc")
-
-ggsave("figures/S80S20.pdf", mk_plt(S80S20_2013, S80S20_2023),
-       width = 9, family = "Times",
-       encoding = "ISOLatin9.enc")
-
-ggsave("figures/ypc.pdf", mk_plt(ypc_2013, ypc_2023),
-       width = 9, family = "Times",
-       encoding = "ISOLatin9.enc")
+save_pdf("figures/ypc_H.pdf", mk_plt(ypc, H))
+save_pdf("figures/ypc_Gini.pdf", mk_plt(ypc, Gini))
+save_pdf("figures/ypc_S80S20.pdf", mk_plt(ypc, S80S20))
+save_pdf("figures/Gini_H.pdf", mk_plt(Gini, H))
+save_pdf("figures/S80S20_H.pdf", mk_plt(S80S20, H))
+save_pdf("figures/Gini.pdf", mk_plt(Gini_2013, Gini_2023))
+save_pdf("figures/H.pdf", mk_plt(H_2013, H_2023))
+save_pdf("figures/S80S20.pdf", mk_plt(S80S20_2013, S80S20_2023))
+save_pdf("figures/ypc.pdf", mk_plt(ypc_2013, ypc_2023))
